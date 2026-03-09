2a3610e162a8aa253f5115c8e003a36b967718ad
SafeWinter <zandong_19@aliyun.com>
2026-03-09 21:54:27 +0800

    wrong study case for shallowReactive

---
+++ P2_quick_start/S05L07_reactivity2/code/diy/vue-project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="container">
+    <div>object.count: {{ obj.count }}</div>
+    <div>object.nested.count: {{ obj.nested.count }}</div>
+  </div>
+</template>
+
+<script setup>
+import { shallowReactive } from 'vue'
+
+let obj = shallowReactive({
+  count: 0,
+  nested: {
+    count: 0
+  }
+})
+
+setTimeout(() => {
+  obj.count++
+  obj.nested.count++
+  console.log('1st update')
+}, 2000)
+
+setTimeout(() => {
+  obj.nested.count = 3
+  console.log('2nd update')
+}, 4000)
+</script>
+
+<style lang="scss" scoped>
+#container {
+  font-size: 3em;
+}
+</style>

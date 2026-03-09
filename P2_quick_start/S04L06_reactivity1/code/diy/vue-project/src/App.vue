397dc4af901e88c3bd86eb66d29e99e2bd7441d8
SafeWinter <zandong_19@aliyun.com>
2026-03-09 20:11:50 +0800

    when ref() accept an object

--- P2_quick_start/S04L06_reactivity1/code/diy/vue-project/src/App.vue
+++ P2_quick_start/S04L06_reactivity1/code/diy/vue-project/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <div>{{ name }}</div>
+  <div>{{ Bill.name }}</div>
+  <div>{{ Bill.age }}</div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 // 现在的 name 就是一个响应式数据
-let name = ref('Bill')
-console.log(name)
-console.log(name.value)
+let Bill = ref({
+  name: 'Biil',
+  age: 18
+})
 setTimeout(() => {
-  name.value = 'Tom'
+  Bill.value.name = 'Biil2'
+  Bill.value.age = 20
 }, 2000)
 </script>
 

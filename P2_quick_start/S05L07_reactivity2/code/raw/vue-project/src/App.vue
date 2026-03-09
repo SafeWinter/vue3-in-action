9f63ae55a8df525741cb3fb9ff25a59982e4a96c
SafeWinter <zandong_19@aliyun.com>
2026-03-09 21:10:37 +0800

    added only source code for 19_Vue3_in_Action:L07:reactivity basics - Part II

---
+++ P2_quick_start/S05L07_reactivity2/code/raw/vue-project/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>{{ count + 1 }}</div>
+    <div>{{ object.id.value + 1 }}</div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const count = ref(0) // 顶级的 Ref 自动解包
+const object = {
+  id: ref(1) // 这就是一个非顶级 Ref 不会自动解包
+}
+</script>
+
+<style lang="scss" scoped></style>

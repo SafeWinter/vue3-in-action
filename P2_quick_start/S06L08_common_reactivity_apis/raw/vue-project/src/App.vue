0afc12a958899e9b95bd3bd194ac4fa16713ab43
SafeWinter <zandong_19@aliyun.com>
2026-03-10 01:14:38 +0800

    added notes for 19_Vue3_in_Action:L08: common reactivity APIs

---
+++ P2_quick_start/S06L08_common_reactivity_apis/raw/vue-project/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { ref, toValue } from 'vue'
+const countRef = ref(10)
+const normalValue = 20
+const getter = ()=>30;
+
+console.log(toValue(countRef)) // 10
+console.log(toValue(normalValue)) // 20
+console.log(toValue(getter)) // 30
+</script>
+
+<style lang="scss" scoped></style>

b9dff44f55f13e4d23723947931abab61f0ecaec
SafeWinter <zandong_19@aliyun.com>
2026-03-09 20:25:41 +0800

    when ref() accept a nested object

--- P2_quick_start/S04L06_reactivity1/code/diy/vue-project/src/App.vue
+++ P2_quick_start/S04L06_reactivity1/code/diy/vue-project/src/App.vue
@@ -1,13 +1,25 @@
 <template>
-  <div class="app">{{ arr }}</div>
+  <div class="app">
+    <div>{{ Bill.name }}</div>
+    <div>{{ Bill.age }}</div>
+    <div>{{ Bill.nested.count }}</div>
+  </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 // 现在的 name 就是一个响应式数据
-let arr = ref([1, 2, 3])
+let Bill = ref({
+  name: 'Biil',
+  age: 18,
+  nested: {
+    count: 1
+  }
+})
 setTimeout(() => {
-  arr.value.push(4, 5, 6)
+  Bill.value.name = 'Gates'
+  Bill.value.age = 28
+  Bill.value.nested.count += 3
 }, 2000)
 </script>
 

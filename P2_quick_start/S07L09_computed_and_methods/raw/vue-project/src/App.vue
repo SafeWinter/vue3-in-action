a6c6c3aba88103118fb2437f80e22922cfcff13a
SafeWinter <zandong_19@aliyun.com>
2026-03-10 01:45:16 +0800

    added notes for 19_Vue3_in_Action:L09: computed properties and methods in Vue 3

---
+++ P2_quick_start/S07L09_computed_and_methods/raw/vue-project/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <button v-on:click="a++">A++</button>
+  <button v-on:click="b++">B++</button>
+  <p>computedA: {{ computedA }}</p>
+  <p>computedB: {{ computedB }}</p>
+  <p>methodA: {{ methodA() }}</p>
+  <p>methodB: {{ methodB() }}</p>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+const a = ref(1)
+const b = ref(1)
+// 创建两个计算属性，分别依赖 a 和 b
+const computedA = computed(() => {
+  console.log('计算属性A重新计算了')
+  return a.value + 1
+})
+const computedB = computed(() => {
+  console.log('计算属性B重新计算了')
+  return b.value + 1
+})
+function methodA() {
+  console.log('methodA执行了')
+  return a.value + 1
+}
+function methodB() {
+  console.log('methodB执行了')
+  return b.value + 1
+}
+</script>
+
+<style lang="scss" scoped></style>

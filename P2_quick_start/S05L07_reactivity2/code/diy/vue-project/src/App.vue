635484e719a8732aca8fd78e426ee4a930b30fd5
SafeWinter <zandong_19@aliyun.com>
2026-03-10 00:27:26 +0800

    updated notes for 19_Vue3_in_Action:L07: add diy code

---
+++ P2_quick_start/S05L07_reactivity2/code/diy/vue-project/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>{{ count + 1 }}</div>
+    <div>{{ object.id + 1 }}</div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const count = ref(0) // 顶级的 Ref 自动解包
+const object = {
+  id: ref(1) // 这就是一个非顶级 Ref 不会自动解包
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+  font-size: 2rem;
+}
+</style>

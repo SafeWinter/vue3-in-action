824853c5608eb7b666bc8ccba79d77c758f2dbf1
SafeWinter <zandong_19@aliyun.com>
2026-03-09 20:30:24 +0800

    test shallowRef

--- P2_quick_start/S04L06_reactivity1/code/diy/vue-project/src/App.vue
+++ P2_quick_start/S04L06_reactivity1/code/diy/vue-project/src/App.vue
@@ -7,20 +7,33 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-// 现在的 name 就是一个响应式数据
-let Bill = ref({
+import { shallowRef } from 'vue'
+
+let Bill = shallowRef({
   name: 'Biil',
   age: 18,
   nested: {
     count: 1
   }
 })
+
 setTimeout(() => {
   Bill.value.name = 'Gates'
   Bill.value.age = 28
   Bill.value.nested.count += 3
+  console.log('1st update')
 }, 2000)
+
+setTimeout(() => {
+  Bill.value = {
+    name: 'Gates',
+    age: 38,
+    nested: {
+      count: 10
+    }
+  }
+  console.log('2nd update')
+}, 4000)
 </script>
 
 <style lang="scss" scoped>

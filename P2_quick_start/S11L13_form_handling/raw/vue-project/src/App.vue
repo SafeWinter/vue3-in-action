22522e0b6e94ede365e382b69d6deaad3543164c
SafeWinter <zandong_19@aliyun.com>
2026-03-12 16:47:29 +0800

    added notes for 19_Vue3_in_Action:P2S11L13: form bindings in Vue3

---
+++ P2_quick_start/S11L13_form_handling/raw/vue-project/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- lazy修饰符演示 -->
+  <input type="text" v-model.lazy="mess1" />
+  <p>你输入的是：{{ mess1 }}</p>
+  <p>类型为{{ typeof mess1 }}</p>
+  <p>长度为{{ mess1.length }}</p>
+
+  <!-- number修饰符演示 -->
+  <input type="text" v-model.number="mess2" />
+  <p>你输入的是：{{ mess2 }}</p>
+  <p>类型为{{ typeof mess2 }}</p>
+  <p>长度为{{ mess2.length }}</p>
+
+  <!-- trim修饰符演示 -->
+  <input type="text" v-model.trim="mess3" />
+  <p>你输入的是：{{ mess3 }}</p>
+  <p>类型为{{ typeof mess3 }}</p>
+  <p>长度为{{ mess3.length }}</p>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const mess1 = ref('')
+const mess2 = ref('')
+const mess3 = ref('')
+</script>
+
+<style scoped></style>

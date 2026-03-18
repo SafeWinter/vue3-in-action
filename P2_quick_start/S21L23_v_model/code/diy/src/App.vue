7829381799fc9593980a19fe851281b3869c723d
SafeWinter <zandong_19@aliyun.com>
2026-03-18 23:33:47 +0800

    added source code for 19_Vue3_in_Action:P2S21L23

---
+++ P2_quick_start/S21L23_v_model/code/diy/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app-container">
+    <h1>请对本次服务评分：</h1>
+    <Rating v-model="rating" />
+    <p v-if="rating > 0">你当前的评价为 {{ rating }} 颗星</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Rating from '@/components/Rating.vue'
+// 这是父组件维护的数据
+const rating = ref(0)
+</script>
+
+<style scoped>
+@import '@/assets/app.css';
+</style>
---
+++ P2_quick_start/S21L23_v_model/code/diy/src/components/Rating.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="rating-container">
+    <span v-for="star in 5" :key="star" class="star" @click="updateRating(star)">
+      {{ rating >= star ? '★' : '☆' }}
+    </span>
+  </div>
+</template>
+
+<script setup>
+const rating = defineModel()
+
+function updateRating(newValue) {
+  rating.value = newValue
+}
+</script>
+
+<style scoped>
+@import '@/assets/rating.css';
+</style>

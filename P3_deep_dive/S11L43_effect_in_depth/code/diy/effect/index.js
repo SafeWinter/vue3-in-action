6f490c28e2bbd5c0eada91551d57b9bb284cf8ac
SafeWinter <zandong_19@aliyun.com>
2026-04-03 13:31:48 +0800

    save env rather than cb

--- P3_deep_dive/S11L43_effect_in_depth/code/diy/effect/index.js
+++ P3_deep_dive/S11L43_effect_in_depth/code/diy/effect/index.js
@@ -3,24 +3,20 @@ const depsMap = new Map(); // 保存依赖关系
 
 function track(target, key) {
   // 建立依赖关系
-  if (activeEffect) {
-    let deps = depsMap.get(key); // 根据属性值去拿依赖的函数集合
-    if (!deps) {
-      deps = new Set(); // 创建一个新的集合
-      depsMap.set(key, deps); // 将集合存入 depsMap
-    }
-    // 将依赖的函数添加到集合里面
-    deps.add(activeEffect);
+  if(!depsMap.has(key)) {
+    depsMap.set(key, new Set()); // 创建一个新的集合
   }
+  let deps = depsMap.get(key); // 根据属性值去拿依赖的函数集合
+
+  // 将依赖的函数添加到集合里面
+  deps.add(activeEffect);
   console.log(depsMap);
 }
 
 function trigger(target, key) {
   // 这里面就需要运行依赖的函数
   const deps = depsMap.get(key);
-  if (deps) {
-    deps.forEach((effect) => effect());
-  }
+  deps && deps.forEach((effect) => effect());
 }
 
 // 原始对象
@@ -33,12 +29,12 @@ const data = {
 const state = new Proxy(data, {
   get(target, key) {
     track(target, key); // 进行依赖收集
-    return target[key];
+    return Reflect.get(target, key);
   },
   set(target, key, value) {
-    target[key] = value;
+    const res = Reflect.set(target, key, value);
     trigger(target, key); // 派发更新
-    return true;
+    return res;
   },
 });
 
@@ -47,14 +43,20 @@ const state = new Proxy(data, {
  * @param {*} fn 回调函数
  */
 function effect(fn) {
-  activeEffect = fn;
-  fn();
-  activeEffect = null;
+  const environment = () => {
+    activeEffect = environment;
+    fn();
+    activeEffect = null;
+  };
+  environment();
 }
 
 effect(() => {
-  // 这里在访问 a 成员时，会触发 get 方法，进行依赖收集
-  console.log(state.a);
-  console.log('执行了函数');
+  if (state.a === 1) {
+    state.b;
+  } else {
+    state.c;
+  }
+  console.log("执行了函数");
 });
 state.a = 10;

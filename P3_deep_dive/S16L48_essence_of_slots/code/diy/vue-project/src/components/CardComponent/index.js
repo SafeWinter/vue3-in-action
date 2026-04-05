3075d593d7c51e19376156bd23beee99d093ad49
SafeWinter <zandong_19@aliyun.com>
2026-04-05 17:05:02 +0800

    handle edge case for empty slot content

--- P3_deep_dive/S16L48_essence_of_slots/code/diy/vue-project/src/components/CardComponent/index.js
+++ P3_deep_dive/S16L48_essence_of_slots/code/diy/vue-project/src/components/CardComponent/index.js
@@ -1,11 +1,17 @@
 import { defineComponent, h } from 'vue'
 import styles from './styles.module.css'
 
+function getHeaderVNode(renderFn) {
+  const vnodeFromParent = renderFn()
+  console.log('vnodeFromParent:', vnodeFromParent);
+  return vnodeFromParent.length === 0 ? h('div', null, '默认标题') : vnodeFromParent
+}
+
 export default defineComponent({
   name: 'CardComponent',
   setup(_, { slots }) {
     console.log('slots:', slots)
-    const headerVNode = slots.header()
+    const headerVNode = getHeaderVNode(slots.header)
     const defaultVNode = slots.default()
     console.log('headerVNode:', headerVNode)
     console.log('defaultVNode', defaultVNode)

a83243546e765956daaa6bb45dcbea111659a4b7
SafeWinter <zandong_19@aliyun.com>
2026-04-05 16:35:43 +0800

    check VNodes returned by slot render fns

--- P3_deep_dive/S16L48_essence_of_slots/code/diy/vue-project/src/components/CardComponent/index.js
+++ P3_deep_dive/S16L48_essence_of_slots/code/diy/vue-project/src/components/CardComponent/index.js
@@ -1,15 +1,18 @@
-import { defineComponent, h } from 'vue';
-import styles from './styles.module.css';
+import { defineComponent, h } from 'vue'
+import styles from './styles.module.css'
 
 export default defineComponent({
   name: 'CardComponent',
   setup(_, { slots }) {
-    console.log('slots:', slots);
-    return () => {
+    console.log('slots:', slots)
+    const headerVNode = slots.header()
+    const defaultVNode = slots.default()
+    console.log('headerVNode:', headerVNode)
+    console.log('defaultVNode', defaultVNode)
+    return () =>
       h('div', { class: styles.card }, [
-        h('div', { class: styles['card-header']}),
-        h('div', { class: styles['card-body']})
+        h('div', { class: styles['card-header'] }, headerVNode),
+        h('div', { class: styles['card-body'] }, defaultVNode)
       ])
-    }
   }
 })

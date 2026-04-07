63d0aaf366c161dfff0449d156f73a1a35798b05
SafeWinter <zandong_19@aliyun.com>
2026-04-07 18:05:12 +0800

    test v-on handler fallthrough

--- P4_more_details/S02L56_fallthrough_attrs/code/diy/src/App.vue
+++ P4_more_details/S02L56_fallthrough_attrs/code/diy/src/App.vue
@@ -1,7 +1,8 @@
 <template>
-  <A class="box" />
+  <A @click="clickHandler" />
 </template>
 
 <script setup>
 import A from './components/A.vue'
+const clickHandler = ({target}) => console.log('(App.vue) clicked target:', target)
 </script>
--- P4_more_details/S02L56_fallthrough_attrs/code/diy/src/components/B.vue
+++ P4_more_details/S02L56_fallthrough_attrs/code/diy/src/components/B.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="b-container">
-  	<p>B组件</p>
+    <p>B组件</p>
   </div>
 </template>
+
+<script setup>
+</script>
+
+<style scoped>
+.b-container {
+  border: 10px solid black;
+}
+p {
+  border: 10px solid grey;
+}
+</style>

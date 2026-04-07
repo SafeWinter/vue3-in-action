ce438e069df0d4951094058670d194a5ee901fa9
SafeWinter <zandong_19@aliyun.com>
2026-04-07 23:17:57 +0800

    tested useFetch composable with yes,no API

---
+++ P4_more_details/S04L58_composables/code/diy/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>当前鼠标位置: {{ x }}, {{ y }}</div>
+  <div class="result">
+    <p v-if="error">{{ error.message }}</p>
+    <template v-else>
+      <p>answer: {{ data.answer }}</p>
+      <img class="img" :src="data.image" alt="image received" />
+    </template>
+    <div><button @click="refresh">Refresh</button></div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useMouse } from './hooks/useMouse'
+import { useFetch } from './hooks/useFetch'
+const { x, y } = useMouse()
+const refresh = () => (url.value = url.value === '' ? 'https://yesno.wtf/api' : '')
+const url = ref('https://yesno.wtf/api')
+const { data, error } = useFetch(url)
+</script>
+
+<style scoped>
+.img {
+  width: 150px;
+}
+</style>

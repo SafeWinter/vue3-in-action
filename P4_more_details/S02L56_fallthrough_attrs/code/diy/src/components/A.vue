5eca545b6cbad73a15b22e7ffe87a5753485e0f1
SafeWinter <zandong_19@aliyun.com>
2026-04-07 20:44:48 +0800

    access attrs in script setup tag

--- P4_more_details/S02L56_fallthrough_attrs/code/diy/src/components/A.vue
+++ P4_more_details/S02L56_fallthrough_attrs/code/diy/src/components/A.vue
@@ -1,5 +1,9 @@
 <template>
-  <header>header</header>
-  <main>main</main>
-  <footer>footer</footer>
+  <h2>组件A</h2>
 </template>
+
+<script setup>
+import { useAttrs } from 'vue'
+const attrs = useAttrs()
+console.log(attrs.onClick);
+</script>

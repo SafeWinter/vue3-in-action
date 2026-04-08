42912d8f0b0da3aecc1b9674159681947466e1a9
SafeWinter <zandong_19@aliyun.com>
2026-04-09 00:30:32 +0800

    added notes for 19_Vue3_in_Action:P4S07L61: transition built-in comp in Vue3

---
+++ P4_more_details/S07L61_transition/code/raw/demo/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+// import App from './App.vue'
+// const app = createApp(App)
+import TuPian from './components/TuPian.vue';
+const app = createApp(TuPian)
+app.mount('#app')
